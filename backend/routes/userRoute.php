922561b2fc3a02177520c4a34f89a23db193bb78
holasoymas <71905956331mahesh@gmail.com>
2024-09-14 18:29:11 +0545

    some minor change to seperate the profile view with user

--- backend/routes/userRoute.php
+++ backend/routes/userRoute.php
@@ -1,14 +1,27 @@
 <?php
-// routers/userRouter.php
+// var_dump($_SERVER["REQUEST_METHOD"]);
+// exit;
+header('Content-Type: application/json');
 require_once '../controllers/userController.php';
 
 $userController = new UserController();
 
+$uid = isset($_GET['uid']) ? $_GET['uid'] : null;
+
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
 if ($requestMethod == 'POST') {
-    $data = json_decode(file_get_contents("php://input"), true);
-    $userController->createUser($data);
+  $data = json_decode(file_get_contents("php://input"), true);
+  $userController->createUser($data);
 }
 
-// Other user-related routes...
+if ($requestMethod == 'GET') {
+  // var_dump($_SERVER["REQUEST_METHOD"]);
+  // exit;
+
+  if ($uid) {
+    $userController->getUserById($uid);
+  } else {
+    echo json_encode(["error" => "User ID (uid) is missing"]);
+  }
+}
