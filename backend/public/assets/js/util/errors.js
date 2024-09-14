737d0de2bb7f9435215744f65558be374bc4272e
holasoymas <71905956331mahesh@gmail.com>
2024-09-14 18:34:42 +0545

    added different responsecode wrapper functions

---
+++ backend/public/assets/js/util/errors.js
@@ -0,0 +1,15 @@
+export function handleBadReqErrors(errorData) {
+  return { status: 400, errors: errorData.errors };
+}
+
+export function handleUnauthenticatedError(errorData) {
+  return { status: 401, error: errorData.error };
+}
+
+export function handleUnauthorizedError() {
+  return { status: 403 };
+}
+
+export function handleUserNotFound(errorData) {
+  return { status: 404, error: errorData.error };
+}
