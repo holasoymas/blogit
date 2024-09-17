134bccbe702dc2de74d2e5ca9bfb2198951e488c
holasoymas <71905956331mahesh@gmail.com>
2024-09-17 22:49:00 +0545

    getBaseDomainUrl function added

--- backend/public/assets/js/config.js
+++ backend/public/assets/js/config.js
@@ -1,7 +1,12 @@
 const config = {
   baseApiUrl: "http://localhost/blogit/backend/routes",
+  baseDomainUrl: "http://localhost/blogit",
 };
 
 export function getBaseApiUrl() {
   return config.baseApiUrl;
+}
+
+export function getBaseDomainUrl() {
+  return config.baseDomainUrl;
 }
