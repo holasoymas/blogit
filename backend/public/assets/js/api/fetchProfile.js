127dfae9976c5cea76dbb176ce6a957b20cec55b
holasoymas <71905956331mahesh@gmail.com>
2024-09-30 22:12:55 +0545

    refactoring the code and seperating profile with user

--- backend/public/assets/js/api/fetchProfile.js
+++ backend/public/assets/js/api/fetchProfile.js
@@ -1,40 +1,20 @@
 import { renderUserProfile } from "../renderer/renderUser.js";
-import { setProfilePicture } from "../util/genprofilepic.js";
 import { renderBlogs } from "../renderer/renderBlog.js";
 import { fetchFromServer } from "../util/fetcher.js";
-import { getUserById } from "./user.js";
+import { renderNav } from "../renderer/renderNav.js";
 
 export async function fetchProfile() {
-    try {
-        const params = new URLSearchParams(window.location.search);
-        const uid = params.get("uid");
+  try {
+    const params = new URLSearchParams(window.location.search);
+    const uid = params.get("uid");
 
-        const json = await fetchFromServer(
-            `profileRoute.php?uid=${encodeURIComponent(uid)}`,
-            "GET",
-        );
+    const json = await fetchFromServer(`profileRoute.php?uid=${encodeURIComponent(uid)}`, "GET");
+    const { loggedInUser, user, blogs } = json;
 
-        const { loggedInUser, user, blogs } = json;
-        const $navProfilePic = document.querySelector(".nav-profile-pic");
-        const $navLoginLink = document.querySelector(".nav-login");
-
-        if (loggedInUser) {
-            const loggedUserData = await getUserById(loggedInUser);
-            console.log(loggedUserData);
-            if (!loggedUserData) {
-                $navProfilePic.classList.add("hidden");
-            } else {
-                $navProfilePic.classList.remove("hidden");
-                const { fname, lname, profile_pic } = loggedUserData;
-                setProfilePicture($navProfilePic, fname, lname, profile_pic);
-            }
-        } else {
-            $navLoginLink.classList.remove("hidden");
-            $navProfilePic.classList.add("hidden");
-        }
-        renderUserProfile(".profile-section", user);
-        renderBlogs(".blog-section", blogs);
-    } catch (err) {
-        console.log(err);
-    }
+    renderNav(loggedInUser);
+    renderUserProfile(".profile-section", user);
+    renderBlogs(".blog-section", blogs);
+  } catch (err) {
+    console.log(err);
+  }
 }
--- backend/public/assets/js/api/user.js
+++ backend/public/assets/js/api/user.js
@@ -3,59 +3,58 @@ import { fetchFromServer } from "../util/fetcher.js";
 import { getBaseDomainUrl } from "../config.js";
 
 export async function registerUser(userData) {
-    try {
-        const res = await fetchFromServer("userRoute.php", "POST", userData);
-        console.log(res);
-        if (res.status === 400) {
-            renderValidationErrors("#register-form", res.errors);
-        }
-        if (res.userId) {
-            window.location.href = `${getBaseDomainUrl()}/profile.html?uid=${encodeURIComponent(
-                res.userId,
-            )}`;
-        }
-    } catch (err) {
-        alert(err.message);
+  try {
+    const res = await fetchFromServer("userRoute.php", "POST", userData);
+    console.log(res);
+    if (res.status === 400) {
+      renderValidationErrors("#register-form", res.errors);
     }
+    if (res.userId) {
+      window.location.href = `${getBaseDomainUrl()}/profile.html?uid=${encodeURIComponent(
+        res.userId,
+      )}`;
+    }
+  } catch (err) {
+    alert(err.message);
+  }
 }
 
 export async function loginUser(userData) {
-    try {
-        const res = await fetchFromServer("login.php", "POST", userData);
-        if (res.status === 400) {
-            alert(res.errors);
-        }
-        if (res.userId) {
-            window.location.href = `${getBaseDomainUrl()}/profile.html?uid=${encodeURIComponent(
-                res.userId,
-            )}`;
-        }
-    } catch (err) {
-        console.log(err);
+  try {
+    const res = await fetchFromServer("login.php", "POST", userData);
+    if (res.status === 400) {
+      alert(res.errors);
     }
+    if (res.userId) {
+      window.location.href = `${getBaseDomainUrl()}/profile.html?uid=${encodeURIComponent(
+        res.userId,
+      )}`;
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function getUserById(uid) {
-    try {
-        const res = await fetchFromServer(`userRoute.php?uid=${encodeURIComponent(uid)}`, "GET");
-        if (res.status === 404) {
-            return null;
-        }
-        return res;
-    } catch (err) {
-        alert(err.message);
-        return null;
-    }
+  try {
+    const res = await fetchFromServer(`userRoute.php?uid=${encodeURIComponent(uid)}`, "GET");
+    if (res.status === 404) return null;
+
+    return res;
+  } catch (err) {
+    alert(err.message);
+    return null;
+  }
 }
 
 export async function logout(e) {
-    e.preventDefault();
-    try {
-        const res = await fetchFromServer("logout.php", "POST");
-        console.log(res, "wolla wolla");
-        if (res.status === "success") window.location.reload();
-        if (res.status === 400) alert(res.errors);
-    } catch (err) {
-        alert("Error occured", err.message);
-    }
+  e.preventDefault();
+  try {
+    const res = await fetchFromServer("logout.php", "POST");
+    console.log(res, "wolla wolla");
+    if (res.status === "success") window.location.reload();
+    if (res.status === 400) alert(res.errors);
+  } catch (err) {
+    alert("Error occured", err.message);
+  }
 }
