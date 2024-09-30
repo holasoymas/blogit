97a11c58fe7cd7fe218162b0c832426b41a3c9e3
holasoymas <71905956331mahesh@gmail.com>
2024-09-30 22:07:53 +0545

    rendering of username instead of id

--- backend/public/assets/js/renderer/renderComments.js
+++ backend/public/assets/js/renderer/renderComments.js
@@ -1,14 +1,24 @@
+import { getUserById } from "../api/user.js";
+import { getBaseDomainUrl } from "../config.js";
+
 export function renderComments($selector, comments) {
   const $commentSection = document.querySelector($selector);
   $commentSection.innerHTML = "";
   if (!comments) {
-    $commentSection.innerHTML = "<h1>No Comments yet, Be the first one to be</h1>";
+    $commentSection.innerHTML = "<h2> 😞 No Comments yet, Be the first one to be</h2>";
     return;
   }
-  comments.forEach((comment) => {
+  comments.forEach(async (comnt) => {
+    const { user_id, comment } = comnt;
+    const user = await getUserById(user_id);
+    console.log(user);
+    if (!user) return;
     const commentHtml = `
         <div class="comment">
-          <p><strong>${comment.user_id}: </strong>${comment.comment}</p>
+          <p><strong> <a href="${getBaseDomainUrl()}/profile.html?uid=${encodeURIComponent(
+            user.id,
+          )}">
+        ${user.fname} ${user.lname}</a>: </strong>${comment}</p>
         </div>
     `;
     $commentSection.insertAdjacentHTML("beforeend", commentHtml);
