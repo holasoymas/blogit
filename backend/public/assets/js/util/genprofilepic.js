26a7970d6cb9a9e5e369aac447f1428b6c3fdec5
holasoymas <71905956331mahesh@gmail.com>
2024-09-30 22:10:39 +0545

    some minor change

--- backend/public/assets/js/util/genprofilepic.js
+++ backend/public/assets/js/util/genprofilepic.js
@@ -1,6 +1,5 @@
 export function setProfilePicture($selector, firstName, lastName, color) {
-  // const $profilePic = document.querySelector($selector);
-  // console.log(color, firstName, lastName);
   $selector.style.backgroundColor = color;
+  // $selector.style.textAlign = "center";
   $selector.innerText = (firstName.charAt(0) + lastName.charAt(0)).toUpperCase();
 }
