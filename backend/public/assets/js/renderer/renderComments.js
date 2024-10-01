7d327699906c86cf64a7699c755e07affb1ac121
holasoymas <71905956331mahesh@gmail.com>
2024-10-01 22:38:45 +0545

    added function to render likes in an post

--- backend/public/assets/js/renderer/renderComments.js
+++ backend/public/assets/js/renderer/renderComments.js
@@ -5,13 +5,13 @@ export function renderComments($selector, comments) {
   const $commentSection = document.querySelector($selector);
   $commentSection.innerHTML = "";
   if (!comments) {
-    $commentSection.innerHTML = "<h2> 😞 No Comments yet, Be the first one to be</h2>";
+    $commentSection.innerHTML = `<h2 style="text-align:center;"> 😞 No Comments yet, Be the first one to be</h2>`;
     return;
   }
   comments.forEach(async (comnt) => {
     const { user_id, comment } = comnt;
     const user = await getUserById(user_id);
-    console.log(user);
+    // console.log(user);
     if (!user) return;
     const commentHtml = `
         <div class="comment">
