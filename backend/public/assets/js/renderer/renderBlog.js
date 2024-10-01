04220879674a1f54f5468347bf69f134bd897f58
holasoymas <71905956331mahesh@gmail.com>
2024-10-01 22:53:16 +0545

    added fun to add or render like also

--- backend/public/assets/js/renderer/renderBlog.js
+++ backend/public/assets/js/renderer/renderBlog.js
@@ -1,6 +1,8 @@
 import { getBaseDomainUrl } from "../config.js";
+import { handleToggleLikeBtn } from "../pages/like.js";
 import { formatDate } from "../util/dateFormatter.js";
 import { setProfilePicture } from "../util/genprofilepic.js";
+import { renderCurrLikeStat } from "./renderLike.js";
 
 export function renderBlogs(selector, blogs) {
   const $container = document.querySelector(selector);
@@ -27,21 +29,31 @@ function renderBlogItem(blog) {
   $template.querySelector(".blog-meta").innerText = `Posted on ${formatDate(blog.created_at)}`;
   $template.querySelector(".blog-content").innerText = blog.content;
 
+  $template.querySelector(".read-more-link").href = `${getBaseDomainUrl()}/blog.html?pid=${
+    blog.pid
+  }`;
+
   return $template;
 }
 
-export function renderFullBlogItem($selector, blog) {
+export async function renderFullBlogItem($selector, blog) {
   const $container = document.querySelector($selector);
-  $container.innerHTML = "";
-  const $fullBlogItem = renderFullBlog(blog);
+  const $fullBlogItem = await renderFullBlog(blog);
   $container.insertAdjacentHTML("beforeend", $fullBlogItem.outerHTML);
+
+  const $likeBtn = document.querySelector(".inner-like-btn svg");
+  console.log($likeBtn);
+  $likeBtn.addEventListener("click", () => {
+    handleToggleLikeBtn(blog.pid);
+  });
 }
 
-function renderFullBlog(blog) {
+async function renderFullBlog(blog) {
   const $template = document
     .querySelector("#full-blog-template")
     .content.firstElementChild.cloneNode(true);
   // console.log($template);
+  // $container.innerHTML = "";
   $template.dataset.blogId = blog.pid;
   $template.querySelector("h1").innerText = blog.title;
   $template.querySelector(".blog-meta").innerText = `Posted on ${formatDate(blog.created_at)}`;
@@ -58,5 +70,10 @@ function renderFullBlog(blog) {
   );
   $authorSection.querySelector(".author-info strong").innerText = `${blog.fname} ${blog.lname}`;
   $authorSection.querySelector(".author-email").innerText = `Email ${blog.email}`;
+
+  // Like part
+  const postId = blog.pid;
+  await renderCurrLikeStat($template, postId);
+
   return $template;
 }
