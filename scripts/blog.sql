979e4d430b9980350d9486335a5ce6c42007964d
holasoymas <71905956331mahesh@gmail.com>
2024-10-03 14:37:06 +0545

    chaning the bid to pid for blogid

--- scripts/blog.sql
+++ scripts/blog.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS blogs (
     pid CHAR(36) PRIMARY KEY DEFAULT (UUID()),
-    uid CHAR(36) NOT NULL,  -- Match the data type with 'id' in users table
+    uid CHAR(36) NOT NULL,  
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
