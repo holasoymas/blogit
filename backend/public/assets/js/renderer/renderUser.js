2d5c270e74eb278e30d9117d80e942bd052f257e
holasoymas <71905956331mahesh@gmail.com>
2024-10-03 14:32:22 +0545

    added user comment,like,joined on date etc

--- backend/public/assets/js/renderer/renderUser.js
+++ backend/public/assets/js/renderer/renderUser.js
@@ -1,3 +1,4 @@
+import { toLocaleStr } from "../util/dateFormatter.js";
 import { setProfilePicture } from "../util/genprofilepic.js";
 
 export function renderUserProfile($container, userData) {
@@ -15,6 +16,11 @@ export function renderUserProfile($container, userData) {
   setProfilePicture($profilePic, userData.fname, userData.lname, userData.profile_pic);
 
   $template.querySelector(".profile-name").innerText = `${userData.fname} ${userData.lname}`;
+  $template.querySelector(".profile-email").innerText = userData.email;
+  $template.querySelector(".profile-blogs").innerText = userData.blog_count;
+  $template.querySelector(".profile-likes").innerText = userData.total_likes;
+  $template.querySelector(".profile-comments").innerText = userData.comment_count;
+  $template.querySelector(".profile-join-date").innerText = `${toLocaleStr(userData.created_at)}`;
 
   $cont.insertAdjacentHTML("beforeend", $template.outerHTML);
 }
