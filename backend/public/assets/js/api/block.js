067d05515b617aee73eb950b40c9587fd512cc23
holasoymas <71905956331mahesh@gmail.com>
2024-10-21 22:12:25 +0545

    selerating the rendering and msg of blocking

--- backend/public/assets/js/api/block.js
+++ backend/public/assets/js/api/block.js
@@ -4,12 +4,29 @@ import { fetchFromServer } from "../util/fetcher.js";
 export async function addBlock(blockData) {
   try {
     const res = await fetchFromServer("blockRoute.php", "POST", blockData);
-    if (res.status === 401) {
+    if (res.status && res.status === 401) {
       alert("Login to request for a block");
       return;
     }
+    if (res.status && res.status === 409) {
+      renderBlock(".block-box", null);
+      return;
+    }
     console.log(res);
-    renderBlock(".block-box", res.isBlockedBy);
+    renderBlock(".block-box", res);
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function hasBlocked(blockTo) {
+  try {
+    const res = await fetchFromServer(`blockRoute.php?blockTo=${blockTo}`, "GET");
+    console.log("isbeing blocked", res);
+    if (res.status && res.status === 404) {
+      return null;
+    }
+    return res;
   } catch (err) {
     console.log(err);
   }
