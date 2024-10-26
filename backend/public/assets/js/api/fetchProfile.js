b7958d71b91b93acfd1ed994b327dc9dde313c92
holasoymas <71905956331mahesh@gmail.com>
2024-10-26 10:58:56 +0545

    rendering dynamcially req. block input(if they have not)

--- backend/public/assets/js/api/fetchProfile.js
+++ backend/public/assets/js/api/fetchProfile.js
@@ -2,6 +2,10 @@ import { renderUserProfile } from "../renderer/renderUser.js";
 import { renderBlogs } from "../renderer/renderBlog.js";
 import { fetchFromServer } from "../util/fetcher.js";
 import { renderNav } from "../renderer/renderNav.js";
+import { renderBlock } from "../renderer/renderBlock.js";
+import { initBlock } from "../pages/block.js";
+import { hasBlocked } from "./block.js";
+import { goNotFoundUser } from "../util/goErrorPages.js";
 
 export async function fetchProfile() {
   try {
@@ -9,11 +13,16 @@ export async function fetchProfile() {
     const uid = params.get("uid");
 
     const json = await fetchFromServer(`profileRoute.php?uid=${encodeURIComponent(uid)}`, "GET");
+    if (json.error && json.status === 404) return goNotFoundUser();
     const { loggedInUser, user, blogs } = json;
-
+    console.log(json);
     renderNav(loggedInUser);
     renderUserProfile(".profile-section", user);
     renderBlogs(".blog-section", blogs);
+    const isBlockedBy = await hasBlocked(uid);
+    renderBlock(".block-box", isBlockedBy);
+    //after rendering only init the click event
+    initBlock();
   } catch (err) {
     console.log(err);
   }
