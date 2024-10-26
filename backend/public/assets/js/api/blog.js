e5952b09d1c79e5b2a5243367f7f1537312c0dc9
holasoymas <71905956331mahesh@gmail.com>
2024-10-26 11:02:27 +0545

    some validation on addblog rendering and other minor change

--- backend/public/assets/js/api/blog.js
+++ backend/public/assets/js/api/blog.js
@@ -1,18 +1,41 @@
-import { getBaseDomainUrl } from "../config.js";
+import { getBaseApiUrl, getBaseDomainUrl } from "../config.js";
 import { renderFullBlogItem } from "../renderer/renderBlog.js";
 import { renderComments } from "../renderer/renderComments.js";
 import { fetchFromServer } from "../util/fetcher.js";
 import { getCommentsByBlog } from "./comment.js";
+import { renderNav } from "../renderer/renderNav.js";
+import { goNotFoundBlog } from "../util/goErrorPages.js";
+import { renderValidationErrors } from "../renderer/renderValidationErrors.js";
 
 export async function addBlog(blogData) {
-  fetchFromServer("blogRoute.php", "POST", blogData)
-    .then((res) => {
-      if (res.status === 401) {
-        alert(res.error);
+  try {
+    const response = await fetch(`${getBaseApiUrl()}/blogRoute.php`, {
+      method: "POST",
+      body: blogData,
+    });
+    // Check if the response is OK
+    if (!response.ok) {
+      const result = await response.json();
+      console.log(result);
+      if (response.status === 401) {
+        alert(result.error);
         window.location.href = `${getBaseDomainUrl()}/login.html`;
+        return;
       }
-    })
-    .catch((err) => alert(err.message));
+      if (response.status === 400) {
+        renderValidationErrors("#blog-form", result.errors);
+        return;
+      }
+    }
+
+    // Parse the response as JSON
+    const result = await response.json();
+    console.log(result);
+    // Handle the response
+  } catch (error) {
+    // Handle errors
+    alert(`An error occurred: ${error.message}`);
+  }
 }
 
 // function to check instantly if the user is authorized to add a blog
@@ -32,13 +55,16 @@ export async function fetchBlogPage() {
   try {
     const params = new URLSearchParams(window.location.search);
     const pid = params.get("pid");
+    if (!pid) goNotFoundBlog();
     const res = await fetchFromServer(`blogRoute.php?pid=${encodeURIComponent(pid)}`, "GET");
+    if (res.status === 404) return goNotFoundBlog();
     const comments = await getCommentsByBlog(pid);
-    console.log(comments);
-    console.log(res);
-    renderFullBlogItem(".blog-content", res);
+    // console.log(comments);
+    // console.log(res);
+    await renderFullBlogItem(".blog-content-area", res.blog);
     renderComments(".comments", comments);
-    // renderFullBlogItem("article", res);
+    // render the nav section dynamic
+    renderNav(res.loggedInUser);
   } catch (err) {
     alert(err);
   }
