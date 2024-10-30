2a3f307512bec008d8dca87de2056c83dd5ad9d5
holasoymas <71905956331mahesh@gmail.com>
2024-10-30 16:02:13 +0545

    sending data based on if the user has blocked the onprofile user or not

--- backend/controllers/blockController.php
+++ backend/controllers/blockController.php
@@ -1,5 +1,4 @@
 <?php
-// controllers/userController.php
 require_once '../models/blockModel.php';
 require_once '../config/db.php';
 require_once '../services/SessionManager.php';
@@ -18,7 +17,7 @@ class BlockController
   {
     if (!SessionManager::isAuthenticated()) {
       http_response_code(401);
-      echo  json_encode(["error" => "Please login first"]);
+      echo json_encode(["error" => "Please login first"]);
       exit;
     }
 
@@ -27,17 +26,56 @@ class BlockController
     $blockTo = $data["blockTo"];
     $blockMsg = $data["blockMsg"];
 
-    $comment = $this->blockModel->createBlock($sessionUser, $blockTo, $blockMsg);
-    http_response_code(201);
-    echo json_encode($comment);
+    try {
+      $hasBlocked = $this->getBlock($blockTo);
+      if ($hasBlocked) {
+        http_response_code(409);
+        echo json_encode(["error" => "You already blocked him/her"]);
+        exit;
+      }
+
+      $block_id = $this->blockModel->createBlock($sessionUser, $blockTo, $blockMsg);
+      if ($block_id) {
+        http_response_code(201);
+        echo json_encode(["block_id" => $block_id]);
+      } else {
+        throw new Exception("Failed to create block.");
+      }
+    } catch (Exception $e) {
+      http_response_code(500);
+      echo json_encode(["error" => $e->getMessage()]);
+    }
     exit;
   }
+
+
   public function getBlock($blockTo)
   {
-    $sessUser = SessionManager::getSession("uid");
-    $blockId = $this->blockModel->getBlock($sessUser, $blockTo);
-    http_response_code(200);
-    echo json_encode($blockId);
+    $sessionUser = SessionManager::getSession("uid");
+    try {
+      $blockId = $this->blockModel->getBlock($sessionUser, $blockTo);
+      return $blockId;
+    } catch (Exception $e) {
+      throw new Exception("Error checking block: " . $e->getMessage());
+    }
+  }
+
+
+  public function getBlockAndRespond($blockTo)
+  {
+    try {
+      $blockId = $this->getBlock($blockTo);
+      if ($blockId) {
+        http_response_code(200);
+        echo json_encode(["block_id" => $blockId]);
+      } else {
+        http_response_code(404);
+        echo json_encode(["error" => "Not Block Yet"]);
+      }
+    } catch (Exception $e) {
+      http_response_code(500);
+      echo json_encode(["error" => $e->getMessage()]);
+    }
     exit;
   }
 }
