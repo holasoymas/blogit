cf0e3b312eff2767bd7abb642073e534125e47dc
holasoymas <71905956331mahesh@gmail.com>
2024-12-12 14:57:06 +0545

    rest api or blog creation added

--- backend/public/assets/js/pages/blog.js
+++ backend/public/assets/js/pages/blog.js
@@ -1,16 +1,52 @@
-import * as Blog from "../api/blog.js";
-document.addEventListener("DOMContentLoaded", Blog.getAddBlogPage);
-document.querySelector("#blog-form").addEventListener("submit", onHandlePost);
+import { getBaseApiUrl } from "../config.js";
+import { renderValidationErrors } from "../renderer/renderValidationErrors.js";
 
-function onHandlePost(e) {
+document.getElementById("uploadForm").addEventListener("submit", async function (e) {
   e.preventDefault();
-  const blogDetails = getBlogDetails();
-  Blog.addBlog(blogDetails);
-}
 
-function getBlogDetails() {
-  return {
-    title: document.querySelector("#title").value,
-    content: document.querySelector("#content").value,
-  };
-}
+  // Clear previous errors
+  renderValidationErrors("#uploadForm", {});
+  const formData = new FormData(this);
+  const progressDiv = document.getElementById("uploadProgress");
+
+  progressDiv.style.display = "block";
+  // progressDiv.textContent = "Uploading...";
+  progressDiv.className = "upload-progress";
+
+  try {
+    const response = await fetch(`${getBaseApiUrl()}/blogRoute.php`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        // If the user is not authenticated, alert them
+        alert("Unauthorized: Please login first.");
+      } else if (response.status === 400) {
+        // If it's a 400 error, it's typically a validation issue
+        const errorData = await response.json();
+        if (errorData.errors) {
+          // Render validation errors if they exist
+          renderValidationErrors("#uploadForm", errorData.errors);
+        }
+      } else {
+        throw new Error("Network response was not ok");
+      }
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    progressDiv.textContent = "Upload successful!";
+    progressDiv.classList.add("success");
+
+    setTimeout(() => {
+      window.location.href = `http://localhost/blogit/blog.html?pid=${data.pid}`;
+    }, 1000);
+  } catch (error) {
+    progressDiv.textContent = "Upload failed: " + error.message;
+    progressDiv.classList.add("error");
+  }
+});
