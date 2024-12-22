88ec981e904d2fc0e25bd71eefc3d2e9233e509f
holasoymas <71905956331mahesh@gmail.com>
2024-12-22 16:50:14 +0545

    added of new report blog feature

--- backend/public/assets/js/api/fetchIndexPage.js
+++ backend/public/assets/js/api/fetchIndexPage.js
@@ -1,4 +1,4 @@
-import { renderBlogs } from "../renderer/renderBlog.js";
+import { renderBlogs, renderPopularBlogs } from "../renderer/renderBlog.js";
 import { renderNav } from "../renderer/renderNav.js";
 import { fetchFromServer } from "../util/fetcher.js";
 
@@ -9,7 +9,45 @@ export async function fetchIndexPage() {
     const { loggedInUser, blogs } = res;
     renderNav(loggedInUser);
     renderBlogs(".blog-section", blogs);
-    console.log(res);
+    renderPopularBlogs(".popular-blogs", blogs);
+    const btns = document.querySelectorAll(".report-blog");
+
+    btns.forEach((btn) => {
+      btn.addEventListener("click", async () => {
+        // console.log("you clicked me ");
+        if (!loggedInUser) {
+          alert("Please login first to report a blog");
+          return;
+        }
+        const reported_blog = btn.closest("[data-blog-id]").dataset.blogId;
+        const report_reason = prompt("Block reason:");
+
+        if (!report_reason || report_reason.trim().length < 10) {
+          alert(
+            !report_reason
+              ? "Please provide a valid reason."
+              : "The reason must be at least 10 characters long.",
+          );
+          return;
+        }
+
+        try {
+          const reported_by = loggedInUser;
+          await fetchFromServer("reportBlog.php", "POST", {
+            reported_blog,
+            reported_by,
+            report_reason,
+          });
+          // console.log(res);
+          alert("Report send successfully");
+        } catch (err) {
+          console.error(err);
+          alert("Something went wrong, please try again later");
+        }
+      });
+    });
+    // console.log(btns);
+    // console.log(res);
   } catch (err) {
     console.error(err);
   }
