ed925c2c32caadc3947303802c808cebad8dece1
holasoymas <71905956331mahesh@gmail.com>
2024-12-23 10:08:39 +0545

    more realistic search added

--- backend/routes/searchRoute.php
+++ backend/routes/searchRoute.php
@@ -1,30 +1,29 @@
 <?php
-
 header("Content-Type: application/json");
 require_once '../config/db.php';
 
 $db = new Database();
 $pdo = $db->connect();
-
-$query = $_GET['query'] ?? ''; // Safely handle missing parameters
+$query = $_GET['query'] ?? '';
 $query = "%$query%";
 
 $sql = "
-    SELECT fname AS suggestion FROM users WHERE fname LIKE :query OR lname LIKE :query
+    SELECT 'uid' as type,id as id, fname as text 
+    FROM users 
+    WHERE fname LIKE :query OR lname LIKE :query
     UNION
-    SELECT title AS suggestion FROM blogs WHERE title LIKE :query
+    SELECT 'pid' as type, pid as id, title as text 
+    FROM blogs 
+    WHERE title LIKE :query
     LIMIT 10
 ";
 
 try {
-  $stmp = $pdo->prepare($sql);
-  $stmp->execute(["query" => $query]);
-
-  $suggestions = $stmp->fetchAll(PDO::FETCH_COLUMN) ?: [];
-  http_response_code(200);
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(["query" => $query]);
+  $suggestions = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
   echo json_encode($suggestions);
 } catch (Exception $e) {
-  // Handle query errors or unexpected exceptions
   http_response_code(500);
   echo json_encode(["error" => "Internal Server Error"]);
 }
