4071713938a7be897de753006e9a3556d40ab2f8
holasoymas <71905956331mahesh@gmail.com>
2024-12-23 10:10:21 +0545

    update and delete btn added with eventlisterner

--- backend/public/assets/js/pages/profile.js
+++ backend/public/assets/js/pages/profile.js
@@ -1,11 +1,44 @@
+import { deleteBlog } from "../api/blog.js";
 import { fetchProfile } from "../api/fetchProfile.js";
 import { logout } from "../api/user.js";
 
 // cange the fetchProfile to call init and inside init fetchProfile fetchBlog etc
 document.addEventListener("DOMContentLoaded", init);
 
-// load user and blogs in the profile
-function init() {
-    fetchProfile();
-    document.querySelector("#logout").addEventListener("click", logout);
+async function init() {
+  document.querySelector("#logout").addEventListener("click", logout);
+  await fetchProfile(); // Wait for fetchProfile to complete
+  attachDeleteListeners();
+  attachUpdateListeners();
+}
+
+function attachDeleteListeners() {
+  const deleteButtons = document.querySelectorAll(".delete-btn button");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", async function () {
+      if (confirm("Are you sure you want to delete this blog?")) {
+        console.log("User confirmed deletion");
+        const article = this.closest("article");
+        const blogId = article.dataset.blogId;
+        await deleteBlog(blogId);
+        window.location.reload();
+      } else {
+        console.log("User canceled deletion");
+      }
+    });
+  });
+}
+
+function attachUpdateListeners() {
+  const deleteButtons = document.querySelectorAll(".update-btn button");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", async function () {
+      console.log("btn click");
+      const article = this.closest("article");
+      const blogId = article.dataset.blogId;
+      window.location.href = `http://localhost/blogit/updateBlog.html?pid=${blogId}`;
+    });
+  });
 }
